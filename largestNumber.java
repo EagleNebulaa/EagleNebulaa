3a78b429b0735c6c389078fca2d014c4b3fc85ff
EagleNebulaa <05pooja.gowda@gmail.com>
2024-09-08 01:03:58 +0530

    Update largestNumber.java

--- largestNumber.java
+++ largestNumber.java
@@ -1,1 +1,13 @@
-
+class HelloWorld {
+    public static void main(String[] args) {
+        int[] arr = {2,6,4,1,8};
+        int max = arr[0];
+        
+        for(int i = 0; i < arr.length-1; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            } 
+        }
+        System.out.println(max);
+    }
+}
